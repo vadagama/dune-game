4b31d600c00ebab97e4aff515052a142e179f581
Oleg Krasnov <oleg@vadagama.ru>
2020-05-27 08:36:24 +0300

    add lesson2

--- desktop/src/com/dune/game/desktop/DesktopLauncher.java
+++ desktop/src/com/dune/game/desktop/DesktopLauncher.java
@@ -2,11 +2,14 @@ package com.dune.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.dune.game.MyGdxGame;
+import com.dune.game.DuneGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
+		config.width = 1280;
+		config.height = 720;
+//		config.foregroundFPS = 1;
+		new LwjglApplication(new DuneGame(), config);
 	}
 }
